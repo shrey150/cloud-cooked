2b319ccc102c2699cbcb4ad84149c9f5d60a5578
nathanhleung <nathanhleung@users.noreply.github.com>
2024-02-17 21:01:51 -0800

    convex fn

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,17 @@
 
 import { api } from "@/convex/_generated/api";
 import { useAction } from "convex/react";
+import { useState } from "react";
 
 export default function Home() {
   const queryFetchAi = useAction(api.functions.queryFetchAi);
+  const [flightNumber, setFlightNumber] = useState("");
 
   return (
     <main>
       <div className="bg-[url('/images/clouds.png')] bg-center bg-cover fixed top-0 left-0 bottom-0 right-0" />
-      <div className="min-h-screen flex items-center justify-center w-full relative p-24 backdrop-blur bg-slate-500/10 text-white text-center">
-        <div className="flex flex-col gap-4 md:max-w-[33%]">
+      <div className="min-h-screen flex items-center justify-center w-full relative p-6 sm:p-12 backdrop-blur bg-slate-500/10 text-white text-center">
+        <div className="flex flex-col gap-4 sm:max-w-[80%] md:max-w-[60%] lg:max-w-[40%]">
           <h1 className="text-5xl font-extrabold">
             am i cooked?
           </h1>
@@ -20,11 +22,21 @@ export default function Home() {
             can make it happen.
           </p>
           <input
-            className="drop-shadow-lg p-3 rounded-xl backdrop-blur bg-white/30 focus:outline-none text-black"
+            className="drop-shadow-lg p-3 rounded-xl backdrop-blur bg-white/80 focus:outline-none text-black"
             type="text"
             placeholder="Enter your flight number"
+            value={flightNumber}
+            onChange={(e) => setFlightNumber(e.target.value)}
           />
-          <button className="drop-shadow-lg bg-gradient-to-r from-sky-500 to-indigo-500 p-2 rounded-xl">Submit</button>
+          <button
+            onClick={() => queryFetchAi({
+              flightNumber,
+            })}
+            className="disabled:opacity-50 hover:opacity-75 drop-shadow-lg bg-gradient-to-r from-sky-500 to-indigo-500 p-2 rounded-xl"
+            disabled={flightNumber === ""}
+          >
+            {flightNumber === "" ? "Enter your flight number" : "Submit"}
+          </button>
         </div>
       </div>
     </main>
--- convex/functions.ts
+++ convex/functions.ts
@@ -4,7 +4,6 @@ import { v } from 'convex/values';
 export const queryFetchAi = action({
   args: { flightNumber: v.string() },
   handler: async (ctx, { flightNumber }) => {
-    const sessionResponse = await fetch("https://agentverse.ai/v1beta1/engine/chat/sessions");
-    console.log(sessionResponse)
+
   }
 })
